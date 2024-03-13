9e62b02ee2ed3dc68689ea2ec7954f9bd3703aad
Thamiris Maria <thamirismariasouzadeoliveira@gmail.com>
2024-03-13 10:33:57 -0300

    desafio 04: solução

---
+++ Desafio04/AnalisadorDePositividadeNumerica.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio04
+{
+    internal class AnalisadorDePositividadeNumerica
+    {
+        double number { get; set; } = 0;
+
+        public AnalisadorDePositividadeNumerica(double number)
+        {
+            this.number = number;
+        }
+
+        public string getPositivoOuNegativo()
+        {
+            if (analisarPositividade())
+            {
+                return "POSITIVO";
+            }
+
+            return "NEGATIVO";
+        }
+
+        public bool analisarPositividade()
+        {
+            if(number == 0)
+            {
+                throw new ArgumentException("Zero não pode ser positivo ou negativo, informe um número válido!");
+            }
+            return number > 0;
+        }
+    }
+}
---
+++ Desafio04/Program.cs
@@ -0,0 +1,27 @@
+﻿
+/*
+* Crie um programa que analise se o valor de uma variável é positivo ou negativo, mostrando essa informação na tela.
+*/
+
+using Desafio04;
+using Helpers;
+
+TituloDoDesafio tituloDoDesafio = new TituloDoDesafio("Calculadora de Operações Básicas");
+tituloDoDesafio.MostrarTituloDoDesafio();
+
+
+Console.Write("Digite um número: ");
+double number = Convert.ToDouble(Console.ReadLine());
+
+AnalisadorDePositividadeNumerica analisador = new AnalisadorDePositividadeNumerica(number);
+
+try
+{
+    string positivoOuNegativo;
+    positivoOuNegativo = analisador.getPositivoOuNegativo();
+    Console.WriteLine($"\nO número {number} é {positivoOuNegativo}");
+}
+catch(ArgumentException e)
+{
+    Console.WriteLine("\n" + e.Message);
+}
