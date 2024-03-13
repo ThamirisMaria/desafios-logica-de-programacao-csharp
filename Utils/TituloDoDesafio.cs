29257d75b1cbd5ee15f58a94ddc64978f2b5bc08
Thamiris Maria <thamirismariasouzadeoliveira@gmail.com>
2024-03-13 10:31:36 -0300

    Initial commit

---
+++ Utils/TituloDoDesafio.cs
@@ -0,0 +1,22 @@
+﻿namespace Helpers;
+using System;
+
+public class TituloDoDesafio
+{
+	string tituloDoDesafio = string.Empty;
+
+	public TituloDoDesafio(string tituloDoDesafio)
+	{
+		this.tituloDoDesafio = tituloDoDesafio;
+	}
+
+	public void MostrarTituloDoDesafio()
+	{
+        int quantidadeDeLetras = tituloDoDesafio.Length;
+        string asteriscos = string.Empty.PadLeft(quantidadeDeLetras, '*');
+
+        Console.WriteLine(asteriscos);
+        Console.WriteLine(tituloDoDesafio);
+        Console.WriteLine(asteriscos + "\n");
+    }
+}
