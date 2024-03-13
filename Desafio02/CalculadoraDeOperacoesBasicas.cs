86d7360c9d8572d75bf7952c2eccd4fb5d6b752d
Thamiris Maria <thamirismariasouzadeoliveira@gmail.com>
2024-03-13 10:33:23 -0300

    desafio 02: solução

---
+++ Desafio02/CalculadoraDeOperacoesBasicas.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio02
+{
+    internal class CalculadoraDeOperacoesBasicas
+    {
+        private double valueOne { get; set; } = 0;
+        private double valueTwo { get; set; } = 0;
+
+        public CalculadoraDeOperacoesBasicas(double valueOne, double valueTwo)
+        {
+            this.valueOne = valueOne;
+            this.valueTwo = valueTwo;
+        }
+
+        public double Somar()
+        {
+            return valueOne + valueTwo;
+        }
+
+        public double Subtrair()
+        {
+            return valueOne - valueTwo;
+        }
+
+        public double Multiplicar()
+        {
+            return valueOne * valueTwo;
+        }
+
+        public double Dividir()
+        {
+            return valueOne / valueTwo;
+        }
+    }
+   
+}
---
+++ Desafio02/Program.cs
@@ -0,0 +1,30 @@
+﻿
+/*
+ Faça um programa que armazene em duas variáveis distintas dois valores distintos: 
+ valueOne e valueTwo. Após, apresente os seguintes cálculos:
+
+    Soma
+    Substração
+    Multiplicação
+    Divisão
+*/
+
+using Desafio02;
+using Helpers;
+
+TituloDoDesafio tituloDoDesafio = new TituloDoDesafio("Calculadora de Operações Básicas");
+tituloDoDesafio.MostrarTituloDoDesafio();
+
+Console.Write("Digite o primeiro valor: ");
+double valueOne = Convert.ToDouble(Console.ReadLine());
+Console.Write("Digite o segundo valor: ");
+double valueTwo = Convert.ToDouble(Console.ReadLine());
+
+CalculadoraDeOperacoesBasicas calculadora = new CalculadoraDeOperacoesBasicas(valueOne, valueTwo);
+
+Console.WriteLine("\nCálculos: \n");
+
+Console.WriteLine($"Soma: {valueOne} + {valueTwo} = {calculadora.Somar()}");
+Console.WriteLine($"Subtração: {valueOne} - {valueTwo} = {calculadora.Subtrair()}");
+Console.WriteLine($"Multiplicação: {valueOne} * {valueTwo} = {calculadora.Multiplicar()}");
+Console.WriteLine($"Divisão: {valueOne} / {valueTwo} = {calculadora.Dividir()}");
