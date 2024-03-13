4cd32b563df6ca4d728db2fdfc806e101e393bc3
Thamiris Maria <thamirismariasouzadeoliveira@gmail.com>
2024-03-13 13:26:24 -0300

    desafio 07: solução

---
+++ Desafio07/OrdenadorDeValores.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio07
+{
+    public class OrdenadorDeValores
+    {
+        private List<double> valores;
+        public List<double> Valores { get { return valores; } }
+
+        public OrdenadorDeValores()
+        {
+            valores = new List<double>();
+        }
+
+        public List<double> GetValoresEmOrdemCrescente()
+        {
+            return valores.OrderBy(valor => valor).ToList();
+        }
+
+    }
+}
---
+++ Desafio07/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Elabore um programa que receba três valores como entrada, e mostre todos na saída em ordem crescente.
+ */
+
+using Desafio07;
+using Helpers;
+
+TituloDoDesafio tituloDoDesafio = new("Ordenador De Valores");
+tituloDoDesafio.MostrarTituloDoDesafio();
+
+OrdenadorDeValores ordenador = new();
+
+for(int i = 1; i <= 3; i++)
+{
+    Console.Write($"Digite o {i}º valor: ");
+    ordenador.Valores.Add(Convert.ToDouble(Console.ReadLine()));
+}
+
+List<double> valoresEmOrdemCrescente = ordenador.GetValoresEmOrdemCrescente();
+
+Console.WriteLine("Valores ordenados em ordem crescente:");
+foreach(double valor in valoresEmOrdemCrescente)
+{
+    Console.WriteLine(valor);
+}
