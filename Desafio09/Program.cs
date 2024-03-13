349abc79f1e514d158810003ca91b1acc6f9393b
Thamiris Maria <thamirismariasouzadeoliveira@gmail.com>
2024-03-13 16:36:01 -0300

    desafio 09: solução

---
+++ Desafio09/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * Crie um algoritmo que tem como entrada um número e imprime a sua tabuada do 1 ao 10.
+ */
+using Helpers;
+
+TituloDoDesafio tituloDoDesafio = new("Calculadora de Tabuada");
+tituloDoDesafio.MostrarTituloDoDesafio();
+
+Console.WriteLine("Digite um número: ");
+double numero = Convert.ToDouble(Console.ReadLine());
+
+List<double> tabuada = CalcularTabuada(numero);
+
+Console.WriteLine($"\nTabuada do número {numero}:\n");
+
+for (int i = 0; i < tabuada.Count; i++)
+{
+    Console.WriteLine($"{numero} * {i+1} = {tabuada[i]}");
+}
+
+static List<double> CalcularTabuada(double numero)
+{
+    List<double> tabuada = [];
+
+    for (int i = 1; i <= 10; i++)
+    {
+        tabuada.Add(numero*i);
+    }
+
+    return tabuada;
+}
