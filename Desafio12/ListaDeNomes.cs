fb0cdbe751dd9de2c632bf66d6ff013028a0f4ef
Thamiris Maria <thamirismariasouzadeoliveira@gmail.com>
2024-03-14 08:56:30 -0300

    desafio 12: solução

---
+++ Desafio12/ListaDeNomes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio12
+{
+    public class ListaDeNomes(List<string> nomes)
+    {
+        private readonly List<string> nomes = nomes;
+
+        public bool ProcurarNome(string nomeParaProcurar)
+        {
+           return nomes.Exists(nome => nome.Equals(nomeParaProcurar, StringComparison.CurrentCultureIgnoreCase));
+        }
+    }
+}
---
+++ Desafio12/Program.cs
@@ -0,0 +1,43 @@
+﻿/*
+ * Dado um array de nomes, faça um programa que tenha como entrada uma expressão textual
+ * a ser pesquisada e retorne se o nome existe ou não no array.
+ */
+using Desafio12;
+using Helpers;
+
+TituloDoDesafio tituloDoDesafio = new("Lista de nomes");
+tituloDoDesafio.MostrarTituloDoDesafio();
+
+List<string> nomes = [];
+
+bool continuar = true;
+while (continuar)
+{
+    Console.WriteLine("Digite um nome para adicionar à lista, ou S para sair: ");
+    string input = Console.ReadLine()!;
+
+    if (input == "s" || input == "S")
+    {
+        continuar = false;
+    }
+    else
+    {
+        nomes.Add(input);
+        Console.Clear();
+    }
+    Console.Clear();
+}
+
+ListaDeNomes listaDeNomes = new(nomes);
+
+Console.Write("Digite um nome para pesquisar: ");
+string nome =  Console.ReadLine()!;
+
+if (listaDeNomes.ProcurarNome(nome))
+{
+    Console.WriteLine($"O nome {nome} existe!");
+}
+else
+{
+    Console.WriteLine($"O nome {nome} não existe!");
+}
