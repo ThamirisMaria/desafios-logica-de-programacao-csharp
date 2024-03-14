596218f14e177951a3879fed068f72052bbcf120
Thamiris Maria <thamirismariasouzadeoliveira@gmail.com>
2024-03-14 12:28:16 -0300

    desafio 10: solução

---
+++ Desafio10/CaixaEletronico.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio10
+{
+    public class CaixaEletronico()
+    {
+        public Dictionary<int, int> Sacar(int valorSaque)
+        {
+            Dictionary<int, int> relacaoDeNotas = OtimizarEntregaDeCedulas(valorSaque);
+
+            if (relacaoDeNotas == null)
+            {
+                throw new ArgumentException("Não é possível sacar o valor informado com as cédulas disponíveis.");
+            }
+
+            return relacaoDeNotas;
+        }
+
+
+        public static Dictionary<int, int> OtimizarEntregaDeCedulas(int valor)
+        {
+            int[] valoresNotas = { 200, 100, 50, 20, 10, 5, 2 };
+            Dictionary<int, int> relacaoDeNotas = new Dictionary<int, int>();
+
+            int restante = valor;
+            foreach (int nota in valoresNotas)
+            {
+                if (restante >= nota)
+                {
+                    int quantidadeNotas = restante / nota;
+                    relacaoDeNotas.Add(nota, quantidadeNotas);
+                    restante %= nota;
+                }
+            }
+
+            if (restante == 0)
+            {
+                return relacaoDeNotas;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ Desafio10/Program.cs
@@ -0,0 +1,43 @@
+﻿/*
+ * Desenvolva um método estático de saque de caixa eletrônico.
+ * 
+ * O usuário deve informar um valor desejado de saque como entrada, 
+ * e esse valor deve ser passado para o método de saque.
+ * 
+ * - O método de saque deve implementar um algoritmo de otimização de entrega de cédulas, 
+ *      calculando o menor número possível de cédulas no qual o valor pode ser decomposto. 
+ * - O método deve retornar a relação de cédulas para o invocador.
+ * - As notas consideradas são de (R$) 200, 100, 50, 20, 10, 5 e 2.
+ * - A relação de cédulas deve ser impressa.
+ */
+
+using Desafio10;
+using Helpers;
+
+TituloDoDesafio tituloDoDesafio = new("Otimização  de Entrega de Cédulas");
+tituloDoDesafio.MostrarTituloDoDesafio();
+
+CaixaEletronico caixaEletronico = new();
+
+Console.Write("Insira o valor para saque: ");
+int valorSaque = int.Parse(Console.ReadLine()!);
+
+
+try
+{
+    Dictionary<int, int> relacaoDeNotas  = caixaEletronico.Sacar(valorSaque);
+
+    Console.Clear();
+
+    Console.WriteLine($"Relação de cédulas para sacar R${valorSaque}:\n");
+    foreach (KeyValuePair<int, int> nota in relacaoDeNotas)
+    {
+        Console.WriteLine($"Cédula de {nota.Key}:\n Quantidade: {nota.Value}\n");
+    }
+}
+catch (ArgumentException e) 
+{ 
+    Console.WriteLine(e.Message);
+}
+
+
