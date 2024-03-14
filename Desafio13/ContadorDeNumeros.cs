98eb4793bb9e8997fa017ee9dc943440a1932f1b
Thamiris Maria <thamirismariasouzadeoliveira@gmail.com>
2024-03-14 08:11:16 -0300

    desafio 13: solução

---
+++ Desafio13/ContadorDeNumeros.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio13
+{
+    public class ContadorDeNumeros
+    {
+        //Dictionary<string, int> quantParesEImpares = new Dictionary<string, int>();
+        //Dictionary<string, int> quantPositivosENegativos = new Dictionary<string, int>();
+
+        private List<double> numeros;
+
+        public ContadorDeNumeros(List<double> numeros)
+        {
+            this.numeros = numeros;
+        }
+
+        bool IsNumeroPar(double numero)
+        {
+            return numero % 2 == 0;
+        }
+
+        bool IsNumeroPositivo(double numero)
+        {
+            return numero > 0;
+        }
+
+        public Dictionary<string, int> GetQuantParesOuImpares()
+        {
+            int contadorPares = 0;
+            int contadorImpares = 0;
+
+            foreach (int numero in numeros)
+            {
+                if (IsNumeroPar(numero))
+                {
+                    contadorPares++;
+                }
+                else
+                {
+                    contadorImpares++;
+                }
+            }
+
+            return new Dictionary<string, int> { { "Pares", contadorPares }, {"Impares", contadorImpares} };
+        }
+
+        public Dictionary<string, int> GetQuantPositivosOuNegativos()
+        {
+            int contadorPositivos = 0;
+            int contadorNegativos = 0;
+
+            foreach (int numero in numeros)
+            {
+                if (IsNumeroPositivo(numero))
+                {
+                    contadorPositivos++;
+                }
+                else
+                {
+                    contadorNegativos++;
+                }
+            }
+
+            return new Dictionary<string, int> { { "Positivos", contadorPositivos }, {"Negativos", contadorNegativos} };
+        }
+    }
+}
---
+++ Desafio13/Program.cs
@@ -0,0 +1,38 @@
+﻿/*
+ * Dado um array de números, mostre quantos valores são pares, 
+ * quantos são ímpares, quantos são positivos e quantos são negativos.
+ */
+
+using Desafio13;
+using Helpers;
+
+TituloDoDesafio tituloDoDesafio = new("Contador de números");
+tituloDoDesafio.MostrarTituloDoDesafio();
+
+List<double> numeros = new();
+
+bool continuar = true;
+while (continuar)
+{
+    Console.WriteLine("Digite um número para adicionar à lista, ou S para sair: ");
+    string input = Console.ReadLine()!;
+
+    if(input == "s" || input == "S")
+    {
+        continuar = false;
+    }
+    else
+    {
+        numeros.Add(Convert.ToDouble(input));
+        Console.Clear();
+    }
+    Console.Clear() ;
+}
+
+ContadorDeNumeros contadorDeNumeros = new(numeros);
+
+Console.WriteLine("\nQuantidade de:\n");
+Console.WriteLine($"{contadorDeNumeros.GetQuantParesOuImpares().First().Key}: {contadorDeNumeros.GetQuantParesOuImpares().First().Value}");
+Console.WriteLine($"{contadorDeNumeros.GetQuantParesOuImpares().ElementAt(1).Key}: {contadorDeNumeros.GetQuantParesOuImpares().ElementAt(1).Value}");
+Console.WriteLine($"{contadorDeNumeros.GetQuantPositivosOuNegativos().First().Key}: {contadorDeNumeros.GetQuantPositivosOuNegativos().First().Value}");
+Console.WriteLine($"{contadorDeNumeros.GetQuantPositivosOuNegativos().ElementAt(1).Key}: {contadorDeNumeros.GetQuantPositivosOuNegativos().ElementAt(1).Value}");
