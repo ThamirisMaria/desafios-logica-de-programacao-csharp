10f9e76b1140d3a59f132cc48da753b57b1d18c3
Thamiris Maria <thamirismariasouzadeoliveira@gmail.com>
2024-03-14 13:34:01 -0300

    desafio 05: solução

---
+++ Desafio05/Jogo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio05
+{
+    public class Jogo(int horaInicial, int horaFinal)
+    {
+
+        public int GetDuracao()
+        {
+            if (horaFinal > horaInicial)
+            {
+                return horaFinal - horaInicial;
+            }
+            else
+            {
+                return 24 - horaInicial + horaFinal;
+            }
+        }
+    }
+}
---
+++ Desafio05/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+ * Faça um programa que receberá a hora inicial e a hora final de um jogo. 
+ * A seguir calcule a duração do jogo, sabendo que o mesmo pode começar em um dia e terminar em outro 
+ * (com duração máxima de 24 horas).
+ * 
+ * Nota: Um jogo tem somente horas "em ponto" - Não é necessário trabalhar com minutos e segundos.
+ */
+
+using Desafio05;
+
+Console.Write("Insira a hora de início do jogo: ");
+int horaInicial = int.Parse(Console.ReadLine()!);
+
+Console.Write("Insira a hora de encerramento do jogo: ");
+int horaFinal = int.Parse(Console.ReadLine()!);
+
+Jogo jogo = new(horaInicial, horaFinal);
+
+Console.WriteLine($"\nA duração do jogo foi de {jogo.GetDuracao()} horas.");
