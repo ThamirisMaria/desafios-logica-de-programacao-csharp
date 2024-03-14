724e6f4814e78dc66ea5c107d5ce67f0b6ff89e3
Thamiris Maria <thamirismariasouzadeoliveira@gmail.com>
2024-03-14 11:26:09 -0300

    desafio 03: solução

---
+++ Desafio03/CalculadoraPagamentoDeCorretores.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio03
+{
+    public class CalculadoraPagamentoDeCorretores(double salarioFixo, double totalDeVendas, double percentualComissao)
+    {
+        double SalarioFixo { get; set; } = salarioFixo;
+        double TotalDeVendas { get; set; } = totalDeVendas;
+        double PercentualComissao { get; set; } = percentualComissao;
+
+        public double CalcularPagamentoTotal()
+        {
+            return SalarioFixo + TotalDeVendas * (PercentualComissao/100);
+        }
+    }
+}
---
+++ Desafio03/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Uma imobiliária paga aos seus corretores um salário fixo por mês 
+ * e uma porcentagem de comissão de acordo com o valor de vendas realizadas. 
+ * Crie um sistema para calcular a soma do salário + a comissão recebida em relação às vendas.
+ * 
+ * - Entradas:
+ *      Valor do salário fixo
+ *      Total de vendas
+ *      Percentual de comissão acordado
+ * - Saídas:
+ *      Valor total a receber
+ *
+ *  Nota: Esse programa deve funcionar para diferentes imobiliárias, ou seja, 
+ *  deve ser possível modificar os valores de entrada e ter o cálculo sendo feito de forma correta.
+ */
+using Desafio03;
+using Helpers;
+
+TituloDoDesafio tituloDoDesafio = new("Calculadora de Pagamento de Corretores");
+tituloDoDesafio.MostrarTituloDoDesafio();
+
+Console.Write("Insira o salário fixo: ");
+double salarioFixo = Convert.ToDouble(Console.ReadLine());
+Console.Write("Insira o total de vendas: ");
+double totalDeVendas =  Convert.ToDouble(Console.ReadLine());
+Console.Write("Insira o percentual de comissão: ");
+double percentualDeComissao =  Convert.ToDouble(Console.ReadLine());
+
+CalculadoraPagamentoDeCorretores calculadoraPagamentoDeCorretores = new(salarioFixo, totalDeVendas, percentualDeComissao);
+
+Console.WriteLine("Valor total a receber: ");
+Console.WriteLine($"R${calculadoraPagamentoDeCorretores.CalcularPagamentoTotal()}");
